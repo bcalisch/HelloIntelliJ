d22c939ac1dbed4ecf28c26dfa859c87faf0b65d
bcalisch <bcalisch@gmail.com>
2015-06-13 16:09:29 -0500

    This is a test

---
+++ src/HelloIntelliJ.java
@@ -0,0 +1,8 @@
+/**
+ * Created by benjamin on 6/13/15.
+ */
+public class HelloIntelliJ {
+    public static void main(String[] args){
+        System.out.println("Hello IntelliJ!, Say What - ");
+    }
+}
---
+++ src/SumCalculator.java
@@ -0,0 +1,13 @@
+/**
+ * Created by benjamin on 6/13/15.
+ */
+public class SumCalculator {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            sum += Integer.parseInt(args[i]);
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
