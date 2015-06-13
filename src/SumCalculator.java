9be451081554705d119644f560876a786ec745e4
bcalisch <bcalisch@gmail.com>
2015-06-13 16:13:23 -0500

    Added some comments

--- src/SumCalculator.java
+++ src/SumCalculator.java
@@ -4,6 +4,7 @@
 public class SumCalculator {
     public static void main(String[] args) {
         int sum = 0;
+        //Putting a comment here!!!
         for (int i = 0; i < args.length; i++) {
             String arg = args[i];
             sum += Integer.parseInt(args[i]);
